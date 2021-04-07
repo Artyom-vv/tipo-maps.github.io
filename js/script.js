f572a03d7c2734b71a0061bc1581b80b0077121e
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2021-04-07 16:56:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -34,4 +34,30 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     // burger end
+
+    // barba
+    function pageTransition() {
+        scroll = window.pageYOffset;
+        document.body.classList.add('fixed');
+        LockScroll();
+        return gsap.timeline().to('ul.transition li', { duration: .4, scaleY: 1, transformOrigin: "bottom left", stagger: 0.2 })
+    };
+    function showPage() {
+        document.body.classList.remove('fixed');
+        UnlockScroll();
+        return gsap.timeline().to('ul.transition li', { duration: .4, scaleY: 0, transformOrigin: "bottom left", stagger: 0.2, delay: 0.15 })
+    };
+    barba.init({
+        sync: true,
+        transitions: [{
+            async leave(data) {
+                await pageTransition();
+                data.current.container.remove()
+            },
+            async enter(data) {
+                await showPage();
+            }
+        }]
+    });
+    // barba end
 });
